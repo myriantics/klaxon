d16711746c38a35d3d8431b29d628e4422477dcd
Myriantics <gmacgaming40@gmail.com>
2024-04-14 10:44:20 -0700

    wip crappy inventory shenanigans (how do i limit stack size :C)

---
+++ src/main/java/net/myriantics/klaxon/datagen/KlaxonItemTagProvider.java
@@ -0,0 +1,30 @@
+package net.myriantics.klaxon.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+import net.myriantics.klaxon.util.KlaxonTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class KlaxonItemTagProvider extends FabricTagProvider.ItemTagProvider {
+
+    public KlaxonItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup arg) {
+        getOrCreateTagBuilder(KlaxonTags.Items.EXTRAPOLATOR_FUEL_REGULAR)
+                .add(Items.GUNPOWDER)
+                .add(Items.FIREWORK_ROCKET);
+        getOrCreateTagBuilder(KlaxonTags.Items.EXTRAPOLATOR_FUEL_SUPER)
+                .add(Items.TNT)
+                .add(Items.FIRE_CHARGE);
+        getOrCreateTagBuilder(KlaxonTags.Items.EXTRAPOLATOR_FUEL_HYPER)
+                .add(Items.END_CRYSTAL)
+                .add(Items.TNT_MINECART);
+    }
+}
