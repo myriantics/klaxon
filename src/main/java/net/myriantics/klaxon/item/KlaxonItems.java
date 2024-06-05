115851b0c113db5a88de8334cb97da82aa5fac85
Gavin <gthemaker032@gmail.com>
2024-06-04 17:49:12 -0700

    added a test item laptops being fucky cba to do this rn o/

---
+++ src/main/java/net/myriantics/klaxon/item/KlaxonItems.java
@@ -0,0 +1,30 @@
+package net.myriantics.klaxon.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import net.myriantics.klaxon.KlaxonMain;
+
+public class KlaxonItems {
+    public static final Item HAMMER = registerItem("hammer", new Item(new FabricItemSettings()));
+
+    private static void addItemsToToolTabGroup(FabricItemGroupEntries entries) {
+        entries.add(HAMMER);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(KlaxonMain.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        KlaxonMain.LOGGER.info("Registering Klaxon's Items");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(KlaxonItems::addItemsToToolTabGroup);
+    }
+}
