ece1b04f9346ea676535eaa39e0ca2fcccc9888d
Gavin <gthemaker032@gmail.com>
2024-06-10 21:38:49 -0700

    added basic stuff for enderpearl plate
    still gotta learn how to properly register entities and then make the plate look like a frisbee

---
+++ src/main/java/net/myriantics/klaxon/item/customitems/EnderPearlPlateItem.java
@@ -0,0 +1,31 @@
+package net.myriantics.klaxon.item.customitems;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import net.myriantics.klaxon.entity.EnderPearlPlateEntity;
+
+public class EnderPearlPlateItem extends Item {
+    public EnderPearlPlateItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack handStack = user.getStackInHand(hand);
+        user.getItemCooldownManager().set(this, 10);
+        if(!world.isClient) {
+            EnderPearlPlateEntity enderPlateEntity = new EnderPearlPlateEntity(world, user);
+            enderPlateEntity.setItem(handStack);
+            enderPlateEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 3.0F, 0.2F);
+        }
+
+        if (!user.getAbilities().creativeMode) {
+            handStack.decrement(1);
+        }
+        return TypedActionResult.success(handStack, world.isClient());
+    }
+}
--- src/main/java/net/myriantics/klaxon/mixin/ModelLoaderMixin.java
+++ src/main/java/net/myriantics/klaxon/mixin/ModelLoaderMixin.java
@@ -22,7 +22,7 @@ public abstract class ModelLoaderMixin {
     protected abstract void addModel(ModelIdentifier modelId);
 
     @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelLoader;addModel(Lnet/minecraft/client/util/ModelIdentifier;)V", ordinal = 3, shift = At.Shift.AFTER))
-    public void addRubyStaff(BlockColors blockColors, Profiler profiler, Map<Identifier, JsonUnbakedModel> jsonUnbakedModels, Map<Identifier, List<ModelLoader.SourceTrackedData>> blockStates, CallbackInfo ci) {
+    public void addHammer(BlockColors blockColors, Profiler profiler, Map<Identifier, JsonUnbakedModel> jsonUnbakedModels, Map<Identifier, List<ModelLoader.SourceTrackedData>> blockStates, CallbackInfo ci) {
         this.addModel(new ModelIdentifier(KlaxonMain.MOD_ID, "hammer-3d", "inventory"));
     }
 }
