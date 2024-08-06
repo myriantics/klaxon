5d83d3c0d10cc85ad4ed15eae09b8dc994742175
Gavin <gthemaker032@gmail.com>
2024-08-06 16:59:44 -0700

    fixed hammer ability modifier to work properly

--- src/main/java/net/myriantics/klaxon/util/AbilityModifierHelper.java
+++ src/main/java/net/myriantics/klaxon/util/AbilityModifierHelper.java
@@ -1,5 +1,6 @@
 package net.myriantics.klaxon.util;
 
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
@@ -13,25 +14,36 @@ public abstract class AbilityModifierHelper {
         float statusEffectModifier = 0.0F;
 
 
-        if (player.getStatusEffect(StatusEffects.STRENGTH) != null) {
-            statusEffectModifier += (0.5F * player.getStatusEffect(StatusEffects.STRENGTH).getAmplifier());
-        }
+        statusEffectModifier += getUnborkedStatusEffectAmplifier(player, StatusEffects.STRENGTH);
 
         // TIL weakness doesn't have a tier 2 version. The more you know
-        if (player.getStatusEffect(StatusEffects.WEAKNESS) != null) {
-            statusEffectModifier -= (0.5F * player.getStatusEffect(StatusEffects.WEAKNESS).getAmplifier());
-            player.sendMessage(Text.literal("but im weak: " + statusEffectModifier));
+        // apparently getAmplifier doesn't actually return the proper amplifier if its a level 1???
+        // GETAMPLIFIER COUNTS UP FROM 0 NVM
+        statusEffectModifier -= getUnborkedStatusEffectAmplifier(player, StatusEffects.WEAKNESS);
+
+        // no negative velocity for you
+        if (statusEffectModifier < 0) {
+            return 0.0F;
         }
 
+        // make it not crazy powerful
         statusEffectModifier *= 0.2F;
+        // make it not a debuff by adding 1
         statusEffectModifier++;
 
         player.sendMessage(Text.literal("test: " + statusEffectModifier));
 
-        if (statusEffectModifier <= 0) {
-            return 0.0F;
-        }
 
         return statusEffectModifier;
+    }
+
+    // fix the mojank crap
+    private static float getUnborkedStatusEffectAmplifier(PlayerEntity player, StatusEffect statusEffect) {
+        if (player != null && player.getStatusEffect(statusEffect) != null) {
+            float amplifier = (float) player.getStatusEffect(statusEffect).getAmplifier();
+
+            return amplifier + 1;
+        }
+        return 0.0F;
     }
 }
