8664ae268da40fa5fbd96be3e76c247d26f97b56
Gavin <gthemaker032@gmail.com>
2024-08-06 17:06:54 -0700

    fixed comments

--- src/main/java/net/myriantics/klaxon/util/AbilityModifierHelper.java
+++ src/main/java/net/myriantics/klaxon/util/AbilityModifierHelper.java
@@ -10,15 +10,11 @@ public abstract class AbilityModifierHelper {
 
         // idk this makes pvp with stuff like the hammer funky
         // weakness arrows go brrt
-        // 0.5 is way too spicy, lets try 0.2 instead
         float statusEffectModifier = 0.0F;
 
 
-        statusEffectModifier += getUnborkedStatusEffectAmplifier(player, StatusEffects.STRENGTH);
-
         // TIL weakness doesn't have a tier 2 version. The more you know
-        // apparently getAmplifier doesn't actually return the proper amplifier if its a level 1???
-        // GETAMPLIFIER COUNTS UP FROM 0 NVM
+        statusEffectModifier += getUnborkedStatusEffectAmplifier(player, StatusEffects.STRENGTH);
         statusEffectModifier -= getUnborkedStatusEffectAmplifier(player, StatusEffects.WEAKNESS);
 
         // no negative velocity for you
@@ -37,7 +33,7 @@ public abstract class AbilityModifierHelper {
         return statusEffectModifier;
     }
 
-    // fix the mojank crap
+    // FYI - getAmplifier() counts up from 0; returning 1 would indicate a level II effect
     private static float getUnborkedStatusEffectAmplifier(PlayerEntity player, StatusEffect statusEffect) {
         if (player != null && player.getStatusEffect(statusEffect) != null) {
             float amplifier = (float) player.getStatusEffect(statusEffect).getAmplifier();
