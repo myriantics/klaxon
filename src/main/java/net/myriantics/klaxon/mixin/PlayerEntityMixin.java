bba9a135a357c26a50554f102b9a2821081551bf
Gavin <gthemaker032@gmail.com>
2024-12-06 14:42:02 -0800

    Fixed hammer damage type mixin being nonfunctional

--- src/main/java/net/myriantics/klaxon/mixin/PlayerEntityMixin.java
+++ src/main/java/net/myriantics/klaxon/mixin/PlayerEntityMixin.java
@@ -1,16 +1,16 @@
 package net.myriantics.klaxon.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.myriantics.klaxon.KlaxonCommon;
 import net.myriantics.klaxon.item.KlaxonItems;
 import net.myriantics.klaxon.util.KlaxonDamageTypes;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Debug(export = true)
 @Mixin(PlayerEntity.class)
@@ -24,21 +24,24 @@ public abstract class PlayerEntityMixin {
     // bl5 is fire aspect
     // bl6 tells if the attack was successful
 
+    // this is how it was in 1.20.1 idk if thats how it works now lol
+
     @ModifyVariable(
             method = "attack",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getVelocity()Lnet/minecraft/util/math/Vec3d;")
     )
     // ordinal 2 selects boolean #3 (bl3)
     // Switches the player's attacking damage type to Hammer Walloping if they crit with a hammer, otherwise uses Hammer Bonking if they do a regular hit.
-    private DamageSource checkCustomAttackType(DamageSource original, @Local(ordinal = 3) boolean willCrit, @Local(ordinal = 0, argsOnly = true) Entity target) {
-        if (original.getAttacker() instanceof PlayerEntity player && player.getMainHandStack().isOf(KlaxonItems.HAMMER)) {
-            if (willCrit) {
-                return KlaxonDamageTypes.hammerWalloping(player);
-            }
+    private DamageSource AttackType(DamageSource value, @Local(ordinal = 2) boolean willCrit) {
 
-            return KlaxonDamageTypes.hammerBonking(player);
+        if (value.getAttacker() instanceof PlayerEntity player && player.getMainHandStack().isOf(KlaxonItems.HAMMER)) {
+            if (willCrit) {
+                value = KlaxonDamageTypes.hammerWalloping(player);
+            } else {
+                value = KlaxonDamageTypes.hammerBonking(player);
+            }
         }
 
-        return original;
+        return value;
     }
 }
