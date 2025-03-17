5143744ae9f414f2fd557ed97008d78b3e1434bd
Myriantics <myriantics@gmail.com>
2025-03-16 21:41:04 -0700

    Ported armor materials

--- src/main/java/net/myriantics/klaxon/item/equipment/armor/KlaxonArmorMaterials.java
+++ src/main/java/net/myriantics/klaxon/item/equipment/armor/KlaxonArmorMaterials.java
@@ -18,51 +18,74 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.function.Supplier;
 
-public class KlaxonArmorMaterials {
-    public static final RegistryEntry<ArmorMaterial> STEEL = register("steel", Util.make(new EnumMap<ArmorItem.Type, Integer>(ArmorItem.Type.class), map -> {
+public class KlaxonArmorMaterials{
+    public static final ArmorMaterial STEEL = register("steel", 27, Util.make(new EnumMap<ArmorItem.Type, Integer>(ArmorItem.Type.class), map -> {
         map.put(ArmorItem.Type.BOOTS, 3);
         map.put(ArmorItem.Type.LEGGINGS, 6);
         map.put(ArmorItem.Type.CHESTPLATE, 8);
         map.put(ArmorItem.Type.HELMET, 3);
-        map.put(ArmorItem.Type.BODY, 11);
-    }), 0, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 1.5f, 0.1f, () -> Ingredient.fromTag(KlaxonItemTags.CRUDE_INCLUSIVE_STEEL_PLATES),
-            List.of(new ArmorMaterial.Layer(KlaxonCommon.locate("steel"))));
+    }), 0, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 1.5f, 0.1f, () -> Ingredient.fromTag(KlaxonItemTags.CRUDE_INCLUSIVE_STEEL_PLATES));
 
-    private static RegistryEntry<ArmorMaterial> register(
+    private static ArmorMaterial register(
             String id,
+            int durabilityMultiplier,
             EnumMap<ArmorItem.Type, Integer> defense,
             int enchantability,
-            RegistryEntry<SoundEvent> equipSound,
+            SoundEvent equipSound,
             float toughness,
             float knockbackResistance,
             Supplier<Ingredient> repairIngredient
     ) {
-        List<ArmorMaterial.Layer> list = List.of(new ArmorMaterial.Layer(Identifier.ofVanilla(id)));
-        return register(id, defense, enchantability, equipSound, toughness, knockbackResistance, repairIngredient, list);
+        return new ArmorMaterial() {
+
+            @Override
+            public int getDurability(ArmorItem.Type type) {
+                return BASE_DURABILITY.get(type) * durabilityMultiplier;
+            }
+
+            @Override
+            public int getProtection(ArmorItem.Type type) {
+                return defense.get(type);
+            }
+
+            @Override
+            public int getEnchantability() {
+                return enchantability;
+            }
+
+            @Override
+            public SoundEvent getEquipSound() {
+                return equipSound;
+            }
+
+            @Override
+            public Ingredient getRepairIngredient() {
+                return repairIngredient.get();
+            }
+
+            @Override
+            public String getName() {
+                return id;
+            }
+
+            @Override
+            public float getToughness() {
+                return toughness;
+            }
+
+            @Override
+            public float getKnockbackResistance() {
+                return knockbackResistance;
+            }
+        };
     }
 
-    private static RegistryEntry<ArmorMaterial> register(
-            String id,
-            EnumMap<ArmorItem.Type, Integer> defense,
-            int enchantability,
-            RegistryEntry<SoundEvent> equipSound,
-            float toughness,
-            float knockbackResistance,
-            Supplier<Ingredient> repairIngredient,
-            List<ArmorMaterial.Layer> layers
-    ) {
-        EnumMap<ArmorItem.Type, Integer> enumMap = new EnumMap(ArmorItem.Type.class);
-
-        for (ArmorItem.Type type : ArmorItem.Type.values()) {
-            enumMap.put(type, defense.get(type));
-        }
-
-        return Registry.registerReference(
-                Registries.ARMOR_MATERIAL,
-                KlaxonCommon.locate(id),
-                new ArmorMaterial(enumMap, enchantability, equipSound, repairIngredient, layers, toughness, knockbackResistance)
-        );
-    }
+    private static final EnumMap<ArmorItem.Type, Integer> BASE_DURABILITY = Util.make(new EnumMap<ArmorItem.Type, Integer>(ArmorItem.Type.class), map -> {
+        map.put(ArmorItem.Type.BOOTS, 13);
+        map.put(ArmorItem.Type.LEGGINGS, 15);
+        map.put(ArmorItem.Type.CHESTPLATE, 16);
+        map.put(ArmorItem.Type.HELMET, 11);
+    });
 
     public static void registerArmorMaterials() {
         KlaxonCommon.LOGGER.info("Registered KLAXON's Armor Materials");
