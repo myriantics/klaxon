b80b2db3f08230fb476fb7d44c98a5cba7b4ce20
Myriantics <myriantics@proton.me>
2025-04-23 18:22:56 -0700

    Added better tag support to blast processor catalyst behaviors. Not sure why anyone would want to tag them, but it's a fun excercise.

--- src/main/java/net/myriantics/klaxon/api/behavior/blast_processor_catalyst/BlastProcessorCatalystBehavior.java
+++ src/main/java/net/myriantics/klaxon/api/behavior/blast_processor_catalyst/BlastProcessorCatalystBehavior.java
@@ -1,11 +1,14 @@
 package net.myriantics.klaxon.api.behavior.blast_processor_catalyst;
 
 import net.minecraft.recipe.input.RecipeInput;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.myriantics.klaxon.block.customblocks.machines.blast_processor.deepslate.DeepslateBlastProcessorBlockEntity;
 import net.myriantics.klaxon.recipe.blast_processing.BlastProcessingRecipeData;
 import net.myriantics.klaxon.recipe.item_explosion_power.ItemExplosionPowerData;
+import net.myriantics.klaxon.registry.KlaxonRegistries;
 
 public interface BlastProcessorCatalystBehavior {
 
@@ -21,6 +24,13 @@ public interface BlastProcessorCatalystBehavior {
 
     boolean shouldRunDispenserEffects(World world, BlockPos pos, DeepslateBlastProcessorBlockEntity blastProcessorBlock, RecipeInput recipeInventory);
 
+    default boolean isIn(TagKey<BlastProcessorCatalystBehavior> tagKey) {
+        return getRegistryEntry().isIn(tagKey);
+    }
+
+    default RegistryEntry<BlastProcessorCatalystBehavior> getRegistryEntry() {
+        return KlaxonRegistries.BLAST_PROCESSOR_BEHAVIORS.getEntry(this);
+    }
 
     // long ass name go brrr
     record BlastProcessorBehaviorItemExplosionPowerEmiDataCompound(double explosionPowerMin, double explosionPowerMax, net.minecraft.text.Text infoText, String path) {}
