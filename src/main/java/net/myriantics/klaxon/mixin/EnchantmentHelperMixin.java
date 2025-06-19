1946670f1e629bd90125e2280d00c5cbdadd67d7
thatsmartguy21 <thatsmartguy21@gmail.com>
2025-06-19 15:27:29 -0700

    Innate Enchantments now are taken into consideration for the EnchantmentHelper.hasAnyEnchantmentsIn() method. This patches Ice both dropping as a block and also spawning Water when broken.

--- src/main/java/net/myriantics/klaxon/mixin/EnchantmentHelperMixin.java
+++ src/main/java/net/myriantics/klaxon/mixin/EnchantmentHelperMixin.java
@@ -3,22 +3,15 @@ package net.myriantics.klaxon.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.math.random.Random;
 import net.myriantics.klaxon.component.configuration.InnateItemEnchantmentsComponent;
 import net.myriantics.klaxon.tag.klaxon.KlaxonItemTags;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
 
 @Mixin(EnchantmentHelper.class)
 public abstract class EnchantmentHelperMixin {
@@ -50,8 +43,7 @@ public abstract class EnchantmentHelperMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getOrDefault(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;")
     )
     private static Object klaxon$forEachEnchantmentOverride1(Object original, @Local(argsOnly = true) ItemStack stack) {
-        InnateItemEnchantmentsComponent innate = InnateItemEnchantmentsComponent.get(stack);
-        if (innate != null) {
+        if (InnateItemEnchantmentsComponent.get(stack) instanceof InnateItemEnchantmentsComponent innate) {
             if (original instanceof ItemEnchantmentsComponent component && !component.isEmpty()) {
                 ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(component);
                 for (RegistryEntry<Enchantment> entry : innate.enchantments().getEnchantments()) {
@@ -71,8 +63,27 @@ public abstract class EnchantmentHelperMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;get(Lnet/minecraft/component/ComponentType;)Ljava/lang/Object;")
     )
     private static Object klaxon$forEachEnchantmentOverride2(Object original, @Local(argsOnly = true) ItemStack stack) {
-        InnateItemEnchantmentsComponent innate = InnateItemEnchantmentsComponent.get(stack);
-        if (innate != null) {
+        if (InnateItemEnchantmentsComponent.get(stack) instanceof InnateItemEnchantmentsComponent innate) {
+            if (original instanceof ItemEnchantmentsComponent component && !component.isEmpty()) {
+                ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(component);
+                for (RegistryEntry<Enchantment> entry : innate.enchantments().getEnchantments()) {
+                    builder.set(entry, innate.getLevel(entry) + component.getLevel(entry));
+                }
+                return builder.build();
+            } else {
+                return innate.enchantments();
+            }
+        }
+
+        return original;
+    }
+
+    @ModifyExpressionValue(
+            method = "hasAnyEnchantmentsIn",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getOrDefault(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;")
+    )
+    private static Object klaxon$hasAnyEnchantmentsInOverride(Object original, @Local(argsOnly = true) ItemStack stack) {
+        if (InnateItemEnchantmentsComponent.get(stack) instanceof InnateItemEnchantmentsComponent innate) {
             if (original instanceof ItemEnchantmentsComponent component && !component.isEmpty()) {
                 ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(component);
                 for (RegistryEntry<Enchantment> entry : innate.enchantments().getEnchantments()) {
