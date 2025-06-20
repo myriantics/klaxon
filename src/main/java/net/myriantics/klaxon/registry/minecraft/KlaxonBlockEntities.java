efa80507234af83117c97ebe46b3e51ea0680664
thatsmartguy21 <thatsmartguy21@gmail.com>
2025-06-20 14:21:58 -0700

    Improved Block Entity registration Deepslate Blast Processor Block Entity now no longer has the "_be" suffix.

--- src/main/java/net/myriantics/klaxon/registry/minecraft/KlaxonBlockEntities.java
+++ src/main/java/net/myriantics/klaxon/registry/minecraft/KlaxonBlockEntities.java
@@ -1,6 +1,7 @@
 package net.myriantics.klaxon.registry.minecraft;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -8,16 +9,13 @@ import net.myriantics.klaxon.KlaxonCommon;
 import net.myriantics.klaxon.block.customblocks.machines.blast_processor.deepslate.DeepslateBlastProcessorBlockEntity;
 
 public class KlaxonBlockEntities {
-    public static final BlockEntityType<DeepslateBlastProcessorBlockEntity> DEEPSLATE_BLAST_PROCESSOR_BLOCK_ENTITY =
-            Registry.register(Registries.BLOCK_ENTITY_TYPE, toBlockEntity(KlaxonBlocks.DEEPSLATE_BLAST_PROCESSOR),
-                    BlockEntityType.Builder.create(DeepslateBlastProcessorBlockEntity::new,
-                            KlaxonBlocks.DEEPSLATE_BLAST_PROCESSOR).build());
+    public static final BlockEntityType<DeepslateBlastProcessorBlockEntity> DEEPSLATE_BLAST_PROCESSOR_BLOCK_ENTITY = register(KlaxonBlocks.DEEPSLATE_BLAST_PROCESSOR, DeepslateBlastProcessorBlockEntity::new);
 
     public static void init() {
         KlaxonCommon.LOGGER.info("Registered KLAXON's Block Entities!");
     }
 
-    public static String toBlockEntity(Block block) {
-        return block.asItem().toString() + "_be";
+    public static <T extends BlockEntity> BlockEntityType<T> register(Block block, BlockEntityType.BlockEntityFactory<T> factory) {
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Registries.BLOCK.getId(block), BlockEntityType.Builder.create(factory, block).build());
     }
 }
