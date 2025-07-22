9667c6a03468b1040c7c9ed28f1ec884ac0353fa
Myriantics <myriantics@proton.me>
2025-07-21 20:40:37 -0700

    Add loot tables for Hallnox Blocks Move errant mixin definition to client block in klaxon.mixins.json

--- src/main/java/net/myriantics/klaxon/datagen/loot_table/KlaxonBlockLootTableProvider.java
+++ src/main/java/net/myriantics/klaxon/datagen/loot_table/KlaxonBlockLootTableProvider.java
@@ -20,22 +20,27 @@ public class KlaxonBlockLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(KlaxonBlocks.NETHER_REACTOR_CORE);
         addDrop(KlaxonBlocks.CRUDE_NETHER_REACTOR_CORE);
 
-        // storage blocks
+        // steel
         addDrop(KlaxonBlocks.STEEL_BLOCK);
-        addDrop(KlaxonBlocks.CRUDE_STEEL_BLOCK);
-        addDrop(KlaxonBlocks.RUBBER_BLOCK);
-        addDrop(KlaxonBlocks.MOLTEN_RUBBER_BLOCK);
-
-        // casings & misc
         addDrop(KlaxonBlocks.STEEL_CASING);
-        addDrop(KlaxonBlocks.CRUDE_STEEL_CASING);
-
-        // plate storage blocks
         addDrop(KlaxonBlocks.STEEL_PLATING_BLOCK);
+        addDrop(KlaxonBlocks.STEEL_DOOR, this::doorDrops);
+        addDrop(KlaxonBlocks.STEEL_TRAPDOOR);
+
+        // crude steel
+        addDrop(KlaxonBlocks.CRUDE_STEEL_BLOCK);
+        addDrop(KlaxonBlocks.CRUDE_STEEL_CASING);
         addDrop(KlaxonBlocks.CRUDE_STEEL_PLATING_BLOCK);
+        addDrop(KlaxonBlocks.CRUDE_STEEL_DOOR, this::doorDrops);
+        addDrop(KlaxonBlocks.CRUDE_STEEL_TRAPDOOR);
+
+        // iron
         addDrop(KlaxonBlocks.IRON_PLATING_BLOCK);
+
+        // gold
         addDrop(KlaxonBlocks.GOLD_PLATING_BLOCK);
 
+        // copper
         addDrop(KlaxonBlocks.COPPER_PLATING_BLOCK);
         addDrop(KlaxonBlocks.EXPOSED_COPPER_PLATING_BLOCK);
         addDrop(KlaxonBlocks.WEATHERED_COPPER_PLATING_BLOCK);
@@ -45,12 +50,31 @@ public class KlaxonBlockLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(KlaxonBlocks.WAXED_WEATHERED_COPPER_PLATING_BLOCK);
         addDrop(KlaxonBlocks.WAXED_OXIDIZED_COPPER_PLATING_BLOCK);
 
+        // rubber
+        addDrop(KlaxonBlocks.RUBBER_BLOCK);
         addDrop(KlaxonBlocks.RUBBER_SHEET_BLOCK);
 
-        // decor
-        addDrop(KlaxonBlocks.STEEL_DOOR, this::doorDrops);
-        addDrop(KlaxonBlocks.STEEL_TRAPDOOR);
-        addDrop(KlaxonBlocks.CRUDE_STEEL_DOOR, this::doorDrops);
-        addDrop(KlaxonBlocks.CRUDE_STEEL_TRAPDOOR);
+        // molten rubber
+        addDrop(KlaxonBlocks.MOLTEN_RUBBER_BLOCK);
+
+        // hallnox
+        addDrop(KlaxonBlocks.HALLNOX_STEM);
+        addDrop(KlaxonBlocks.HALLNOX_HYPHAE);
+        addDrop(KlaxonBlocks.STRIPPED_HALLNOX_STEM);
+        addDrop(KlaxonBlocks.STRIPPED_HALLNOX_HYPHAE);
+        addDrop(KlaxonBlocks.HALLNOX_PLANKS);
+        addDrop(KlaxonBlocks.HALLNOX_STAIRS);
+        addDrop(KlaxonBlocks.HALLNOX_SLAB);
+        addDrop(KlaxonBlocks.HALLNOX_FENCE);
+        addDrop(KlaxonBlocks.HALLNOX_FENCE_GATE);
+        addDrop(KlaxonBlocks.HALLNOX_DOOR, this::doorDrops);
+        addDrop(KlaxonBlocks.HALLNOX_TRAPDOOR);
+        addDrop(KlaxonBlocks.HALLNOX_PRESSURE_PLATE);
+        addDrop(KlaxonBlocks.HALLNOX_BUTTON);
+        addDrop(KlaxonBlocks.HALLNOX_POD);
+        flowerbedDrops(KlaxonBlocks.POTTED_HALLNOX_POD);
+        addDrop(KlaxonBlocks.HALLNOX_WART_BLOCK);
+        addDrop(KlaxonBlocks.HALLNOX_SIGN);
+        addDrop(KlaxonBlocks.HALLNOX_HANGING_SIGN);
     }
 }
