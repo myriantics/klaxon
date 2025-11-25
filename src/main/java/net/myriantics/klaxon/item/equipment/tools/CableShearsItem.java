40ecb63c1e59d759996c9e40d3d9867858ba80c4
Myriantics <myriantics@gmail.com>
2025-11-25 12:17:11 -0800

    CableShearsItem now extends ShearsItem to improve compatibility.

--- src/main/java/net/myriantics/klaxon/item/equipment/tools/CableShearsItem.java
+++ src/main/java/net/myriantics/klaxon/item/equipment/tools/CableShearsItem.java
@@ -1,12 +1,17 @@
 package net.myriantics.klaxon.item.equipment.tools;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.component.type.ToolComponent;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.*;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.ActionResult;
 import net.myriantics.klaxon.component.ability.InstabreakingToolComponent;
 import net.myriantics.klaxon.component.configuration.ToolUseRecipeConfigComponent;
@@ -14,18 +19,26 @@ import net.myriantics.klaxon.registry.item.KlaxonDataComponentTypes;
 import net.myriantics.klaxon.registry.misc.KlaxonSoundEvents;
 import net.myriantics.klaxon.tag.klaxon.KlaxonBlockTags;
 
-public class CableShearsItem extends MiningToolItem {
+import java.util.ArrayList;
 
-    public static final ToolComponent SHEARS_TOOL_COMPONENT = ShearsItem.createToolComponent();
+public class CableShearsItem extends ShearsItem {
 
-    public static final float BASE_ATTACK_DAMAGE = 3.0F;
-    public static final float BASE_ATTACK_SPEED = -2.6F;
+    private final ToolMaterial material;
 
     public CableShearsItem(ToolMaterial material, Settings settings) {
-        super(material, KlaxonBlockTags.CABLE_SHEARS_MINEABLE, settings
+        super(settings
+                .component(DataComponentTypes.TOOL, tupleToolComponent(ShearsItem.createToolComponent(), material, KlaxonBlockTags.CABLE_SHEARS_MINEABLE))
                 .component(KlaxonDataComponentTypes.TOOL_USE_RECIPE_CONFIG, new ToolUseRecipeConfigComponent(KlaxonSoundEvents.ITEM_CABLE_SHEARS_USAGE))
                 .component(KlaxonDataComponentTypes.INSTABREAK_TOOL_COMPONENT, new InstabreakingToolComponent(KlaxonBlockTags.CABLE_SHEARS_INSTABREAKABLE))
         );
+        this.material = material;
+    }
+
+    private static ToolComponent tupleToolComponent(ToolComponent shears, ToolMaterial material, TagKey<Block> effectiveBlocks) {
+        ArrayList<ToolComponent.Rule> rules = new ArrayList<>(shears.rules());
+        ToolComponent fromMaterial = material.createComponent(effectiveBlocks);
+        rules.addAll(fromMaterial.rules());
+        return new ToolComponent(rules, fromMaterial.defaultMiningSpeed(), fromMaterial.damagePerBlock());
     }
 
     public static AttributeModifiersComponent createAttributeModifiers(ToolMaterial material, float baseAttackDamage, float attackSpeed) {
@@ -42,24 +55,13 @@ public class CableShearsItem extends MiningToolItem {
                 ).build();
     }
 
-    // I... am Shears.
-
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        ActionResult shearsResult = Items.SHEARS.useOnBlock(context);
-        return shearsResult.equals(ActionResult.PASS) ? super.useOnBlock(context) : shearsResult;
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        return true;
     }
 
     @Override
-    public float getMiningSpeed(ItemStack stack, BlockState state) {
-        float shearsSpeed = SHEARS_TOOL_COMPONENT.getSpeed(state);
-        float cableShearsSpeed = super.getMiningSpeed(stack, state);
-        return Math.max(shearsSpeed, cableShearsSpeed);
-    }
-
-    @Override
-    public boolean isCorrectForDrops(ItemStack stack, BlockState state) {
-        if (SHEARS_TOOL_COMPONENT.isCorrectForDrops(state)) return true;
-        return super.isCorrectForDrops(stack, state);
+    public void postDamageEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        stack.damage(2, attacker, EquipmentSlot.MAINHAND);
     }
 }
