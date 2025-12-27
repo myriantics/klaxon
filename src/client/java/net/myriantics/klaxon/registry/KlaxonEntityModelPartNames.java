fb22545f9bc0fd1c53457139decda26123905502
thatsmartguy21 <thatsmartguy21@gmail.com>
2025-12-26 21:58:26 -0800

    Added a somewhat proper model to the Crested Steel Helmet

--- src/client/java/net/myriantics/klaxon/registry/KlaxonEntityModelPartNames.java
+++ src/client/java/net/myriantics/klaxon/registry/KlaxonEntityModelPartNames.java
@@ -9,6 +9,7 @@ public abstract class KlaxonEntityModelPartNames {
     public static final String HELMET_CREST_BASE_UPPER = register("helmet_crest_base_upper");
     public static final String HELMET_CREST_BASE_BACK = register("helmet_crest_base_back");
     public static final String HELMET_CREST_UPPER_SUPPORT = register("helmet_crest_upper_support");
+    public static final String HELMET_CREST_EDGE_SUPPORT = register("helmet_crest_edge_support");
     public static final String HELMET_CREST_BACK_SUPPORT = register("helmet_crest_back_support");
 
     private static String register(String name) {
--- src/client/java/net/myriantics/klaxon/render/model/HelmetCrestEntityModel.java
+++ src/client/java/net/myriantics/klaxon/render/model/HelmetCrestEntityModel.java
@@ -13,23 +13,22 @@ public class HelmetCrestEntityModel<T extends LivingEntity> extends AnimalModel<
     private final ImmutableList<ModelPart> headParts;
 
     private final ModelPart crestUpper;
-    /*
     private final ModelPart crestBack;
     private final ModelPart crestBaseUpper;
     private final ModelPart crestBaseBack;
     private final ModelPart upperSupport;
-    private final ModelPart backSupport;*/
+    private final ModelPart edgeSupport;
+    private final ModelPart backSupport;
 
     public HelmetCrestEntityModel(ModelPart root) {
         this.crestUpper = root.getChild(KlaxonEntityModelPartNames.HELMET_CREST_UPPER);
-        /*
         this.crestBack = root.getChild(KlaxonEntityModelPartNames.HELMET_CREST_BACK);
         this.crestBaseUpper = root.getChild(KlaxonEntityModelPartNames.HELMET_CREST_BASE_UPPER);
         this.crestBaseBack = root.getChild(KlaxonEntityModelPartNames.HELMET_CREST_BASE_BACK);
         this.upperSupport = root.getChild(KlaxonEntityModelPartNames.HELMET_CREST_UPPER_SUPPORT);
+        this.edgeSupport = root.getChild(KlaxonEntityModelPartNames.HELMET_CREST_EDGE_SUPPORT);
         this.backSupport = root.getChild(KlaxonEntityModelPartNames.HELMET_CREST_BACK_SUPPORT);
-         */
-        this.headParts = ImmutableList.of(this.crestUpper);
+        this.headParts = ImmutableList.of(this.crestUpper, this.crestBack, this.crestBaseUpper, this.crestBaseBack, this.upperSupport, this.edgeSupport, this.backSupport);
     }
 
     @Override
@@ -43,38 +42,53 @@ public class HelmetCrestEntityModel<T extends LivingEntity> extends AnimalModel<
         Dilation dilation = new Dilation(1.0F);
         modelPartData.addChild(
                 KlaxonEntityModelPartNames.HELMET_CREST_UPPER,
-                ModelPartBuilder.create().uv(0, 32).cuboid(-1f, 6f, -4f, 2, 3, 16, dilation),
+                ModelPartBuilder.create()
+                        .uv(0, 32)
+                        .cuboid(-1.501f, 2.8122f, -7.8115f, 3.002f, 3.751f, 15f, dilation),
                 ModelTransform.NONE
         );
-        /*
         modelPartData.addChild(
                 KlaxonEntityModelPartNames.HELMET_CREST_BACK,
-                ModelPartBuilder.create(),
+                ModelPartBuilder.create()
+                        .uv(0, 32)
+                        .cuboid(-1.5f, -7.1888f, 3.4375f, 3f, 13.751f, 3.75f, dilation),
                 ModelTransform.NONE
         );
         modelPartData.addChild(
                 KlaxonEntityModelPartNames.HELMET_CREST_BASE_UPPER,
-                ModelPartBuilder.create(),
+                ModelPartBuilder.create()
+                        .uv(0, 32)
+                        .cuboid(-2.501f, 1.5622f, -7.8125f, 5f, 1.251f, 11.251f, dilation),
                 ModelTransform.NONE
         );
         modelPartData.addChild(
                 KlaxonEntityModelPartNames.HELMET_CREST_BASE_BACK,
-                ModelPartBuilder.create(),
+                ModelPartBuilder.create()
+                        .uv(0, 32)
+                        .cuboid(-2.5f, -7.1878f, 2.1875f, 5f, 10f, 1.25f, dilation),
                 ModelTransform.NONE
         );
         modelPartData.addChild(
                 KlaxonEntityModelPartNames.HELMET_CREST_UPPER_SUPPORT,
-                ModelPartBuilder.create(),
+                ModelPartBuilder.create()
+                        .uv(0, 32)
+                        .cuboid(-2.5f, 0.3113f, -7.8125f, 5f, 1.25f, 5f, dilation),
+                ModelTransform.NONE
+        );
+        modelPartData.addChild(
+                KlaxonEntityModelPartNames.HELMET_CREST_EDGE_SUPPORT,
+                ModelPartBuilder.create()
+                        .uv(0, 32)
+                        .cuboid(-2.499f, -0.9378f, -0.3125f, 5f, 3.75f, 3.75f, dilation),
                 ModelTransform.NONE
         );
         modelPartData.addChild(
                 KlaxonEntityModelPartNames.HELMET_CREST_BACK_SUPPORT,
-                ModelPartBuilder.create(),
+                ModelPartBuilder.create()
+                        .uv(0, 32)
+                        .cuboid(-2.5f, -7.1878f, 0.9365f, 5f, 5f, 1.25f, dilation),
                 ModelTransform.NONE
         );
-
-
-         */
 
         return TexturedModelData.of(modelData, 32, 32);
     }
