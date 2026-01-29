b724703a172b668ae8b70800764673cbc780998b
myriantics <myriantics@proton.me>
2026-01-29 02:36:56 -0800

    Fixed Infinirepair mixin so that compiler doesn't complain. It also uses Slices now.

--- src/main/java/net/myriantics/klaxon/mixin/minecraft/anvil_adjustments/infinirepair/AnvilScreenHandlerMixin.java
+++ src/main/java/net/myriantics/klaxon/mixin/minecraft/anvil_adjustments/infinirepair/AnvilScreenHandlerMixin.java
@@ -9,20 +9,21 @@ import net.myriantics.klaxon.tag.klaxon.KlaxonItemTags;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class AnvilScreenHandlerMixin {
     @WrapOperation(
             method = "updateResult",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;set(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;",
-                    ordinal = 1)
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/screen/AnvilScreenHandler;getNextCost(I)I"),
+                    to = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;set(Lnet/minecraft/item/ItemStack;Lnet/minecraft/component/type/ItemEnchantmentsComponent;)V")
+            ),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;set(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;")
     )
     private <T> T klaxon$anvilUsesOverride(ItemStack instance, ComponentType<? super T> type, @Nullable T value, Operation<T> original) {
-        // crappy way of overwriting anvil uses
-        if (instance.isIn(KlaxonItemTags.INFINITELY_REPAIRABLE)) {
-            return (T) Integer.valueOf(0);
-        }
-        // this throws an error in IDE but it runs fine and has caused no problems... this is fine...
-        return original.call(instance, type, value);
+        return instance.isIn(KlaxonItemTags.INFINITELY_REPAIRABLE)
+                ? value
+                : original.call(instance, type, value);
     }
 }
