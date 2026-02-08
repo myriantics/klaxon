cd8d8599b7f6893e1a13171ce5daa24bd3184684
myriantics <myriantics@proton.me>
2026-02-07 18:35:07 -0800

    Removed unused gamerule Update changelog

--- src/main/java/net/myriantics/klaxon/registry/misc/KlaxonGameRules.java
+++ src/main/java/net/myriantics/klaxon/registry/misc/KlaxonGameRules.java
@@ -14,10 +14,6 @@ public abstract class KlaxonGameRules {
             "dispensersPerformWorldItemInteractionRecipes", GameRules.Category.MISC, true
     );
 
-    public static final GameRules.Key<GameRules.IntRule> GRAPPLE_CLAW_VEINMINE_RADIUS = registerIntRule(
-            "grappleClawVeinmineRadius", GameRules.Category.PLAYER, 8, 0, 64
-    );
-
     private static GameRules.Key<GameRules.BooleanRule> registerBooleanRule(String name, GameRules.Category category, boolean defaultValue) {
         return GameRuleRegistry.register(KlaxonCommon.locateAlt(name), category, GameRuleFactory.createBooleanRule(defaultValue));
     }
