2f367e7db08a3deec1d51677c16004af1594fd86
myriantics <myriantics@proton.me>
2026-04-29 21:48:52 -0700

    Precision Dispenser now no longer dispenses items in random directions when using the default dispenser behavior

--- src/main/java/net/myriantics/klaxon/mixin/minecraft/precision_dispenser/DefaultDispenseItemBehaviorMixin.java
+++ src/main/java/net/myriantics/klaxon/mixin/minecraft/precision_dispenser/DefaultDispenseItemBehaviorMixin.java
@@ -3,8 +3,16 @@ package net.myriantics.klaxon.mixin.minecraft.precision_dispenser;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Position;
 import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.myriantics.klaxon.block.machines.precision_dispenser.PrecisionDispenserBlock;
+import net.myriantics.klaxon.block.machines.precision_dispenser.PrecisionDispenserBlockEntity;
 import net.myriantics.klaxon.mechanics.muffling.MufflableBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +28,18 @@ public abstract class DefaultDispenseItemBehaviorMixin {
             // bonked
         } else {
             original.call(instance, blockSource);
+        }
+    }
+
+    @WrapOperation(
+            method = "execute",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/core/dispenser/DefaultDispenseItemBehavior;spawnItem(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;ILnet/minecraft/core/Direction;Lnet/minecraft/core/Position;)V")
+    )
+    private void klaxon$dispenseItemsPreciselyIfNeeded(Level level, ItemStack stack, int speed, Direction facing, Position position, Operation<Void> original, @Local(argsOnly = true) BlockSource source) {
+        if (source.blockEntity() instanceof PrecisionDispenserBlockEntity) {
+            original.call(level, stack, 0, facing, position);
+        } else {
+            original.call(level, stack, speed, facing, position);
         }
     }
 }
