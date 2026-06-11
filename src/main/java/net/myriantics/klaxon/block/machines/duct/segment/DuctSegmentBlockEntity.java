7d31931c1c94dc3d3e4209c6897bc77aad8c8432
myriantics <myriantics@proton.me>
2026-06-11 15:26:24 -0700

    Fixed Ducts not replacing empty payloads at the end of a duct pushing chain

--- src/main/java/net/myriantics/klaxon/block/machines/duct/segment/DuctSegmentBlockEntity.java
+++ src/main/java/net/myriantics/klaxon/block/machines/duct/segment/DuctSegmentBlockEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.myriantics.klaxon.KlaxonCommon;
 import net.myriantics.klaxon.block.machines.duct.BaseDuctComponentBlockEntity;
 import net.myriantics.klaxon.mechanics.logistics.itemduct.DuctInteractionHandler;
 import net.myriantics.klaxon.mechanics.logistics.itemduct.DuctNode;
@@ -41,6 +42,10 @@ public class DuctSegmentBlockEntity extends BaseDuctComponentBlockEntity {
     public boolean push(DuctPayload payload, Direction inputFace, int remainingDepth) {
         // if we can fully push our contents into output and there's recursion left to do, return true
         if (remainingDepth != 0 && this.getOrInitHandler().push(this.getPayload(), inputFace.getOpposite(), remainingDepth - 1)) {
+            return true;
+        }
+        // if we can't move forwards, check if we're fully empty and replace payload if so
+        if (!this.hasNonEmptyPayload()) {
             return true;
         }
         // if we were unable to fully push our contents out, return false to signify that a new payload should not replace content
